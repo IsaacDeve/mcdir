06725d07476135476007e6ae889201aa043295f0
IsaacDeve <156547171+IsaacDeve@users.noreply.github.com>
2024-10-10 21:06:25 -0700

    1.0

--- main.lua
+++ main.lua
@@ -1,14 +1,1 @@
-local mcd = require("mcdir.mcdir")
-
-function love.load()
-    love.window.setMode(800, 600)
-    mcd.tileSize = 64
-end
-
-function love.update(dt)
-
-end
-
-function love.draw()
-    mcd.
-end
+--Go to mcdir/mcdir.lua
